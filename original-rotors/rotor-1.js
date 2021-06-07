be3d23436703ffd8be0f35374f0ee5e9a30726d3
Chance Harmon <henrikillsit@gmail.com>
2021-06-07 14:31:10 -0700

    have basic output func for a rotor, reflectors setup as well'

--- original-rotors/rotor-1.js
+++ original-rotors/rotor-1.js
@@ -3,38 +3,25 @@
 // Not quite sure how to use these the best, but want to get the orginal wirings down in code
 
 const rotorOne = {
-    start_position: 'A',
+    start_position: 0,
     offset: {
         start_letter: null,
         amount: null
     },
-    positions: {
-        'A': 'E',
-        'B': 'K',
-        'C': 'M',
-        'D': 'F',
-        'E': 'L',
-        'F': 'G',
-        'G': 'D',
-        'H': 'Q',
-        'I': 'V',
-        'J': 'Z',
-        'K': 'N',
-        'L': 'T',
-        'M': 'O',
-        'N': 'W',
-        'O': 'Y',
-        'P': 'H',
-        'Q': 'X',
-        'R': 'U',
-        'S': 'S',
-        'T': 'P',
-        'U': 'A',
-        'V': 'I',
-        'W': 'B',
-        'X': 'R',
-        'Y': 'C',
-        'Z': 'J'
-    }
+    positions: [
+        ['A', 'E'], ['B', 'K'],
+        ['C', 'M'], ['D', 'F'],
+        ['E', 'L'], ['F', 'G'],
+        ['G', 'D'], ['H', 'Q'],
+        ['I', 'V'], ['J', 'Z'],
+        ['K', 'N'], ['L', 'T'],
+        ['M', 'O'], ['N', 'W'],
+        ['O', 'Y'], ['P', 'H'],
+        ['Q', 'X'], ['R', 'U'],
+        ['S', 'S'], ['T', 'P'],
+        ['U', 'A'], ['V', 'I'],
+        ['W', 'B'], ['X', 'R'],
+        ['Y', 'C'], ['Z', 'J']
+    ]
 }
 module.exports = rotorOne;
---
+++ reflectors/reflectorB.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const reflectorB = {
+    'A': 'Y',
+    'B': 'R',
+    'C': 'U',
+    'D': 'H',
+    'E': 'Q',
+    'F': 'S',
+    'G': 'L',
+    'I': 'P',
+    'J': 'X',
+    'K': 'N',
+    'M': 'O',
+    'T': 'Z',
+    'V': 'W'
+}
+module.exports = reflectorB;
