d912711d57236b74853bb99a7e92617924ca8c62
Chance Harmon <henrikillsit@gmail.com>
2021-06-09 14:35:29 -0700

    offset now working for both negative and positive offsets

--- utilities/inverseRotor.js
+++ utilities/inverseRotor.js
@@ -1,5 +1,6 @@
 'use strict';
-
+//This function is used to swap indexs of a rotor(key values)
+//Useful for creating the output array from an input rotor array
 const inverseRotor = arr => {
     const newArr = arr.map(item => {
         return [item[1], item[0]]
