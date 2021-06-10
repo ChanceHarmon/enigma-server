d3c8925d957aabf32e71fdd97c41ba894622b0ea
Chance Harmon <henrikillsit@gmail.com>
2021-06-09 19:13:19 -0700

    rotate rotor counter clockwise complete

---
+++ utilities/rotorRotateCCW.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// const rotorI = require('../original-rotors/rotorI.js')
+
+const rotorRotateCCW = rotor => {
+
+    let tempIn = rotor.positions.inputSchedule.shift()
+    rotor.positions.inputSchedule.push(tempIn)
+
+    let tempOut = rotor.positions.outputSchedule.shift()
+    rotor.positions.outputSchedule.push(tempOut)
+
+    return rotor
+}
+
+module.exports = rotorRotateCCW;
+
+// console.log(rotorRotateCCW(rotorI))
