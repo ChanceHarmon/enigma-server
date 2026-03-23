bae8894bf7dd5d9f6be4c8fc0e9801ae9f933b03
Chance Harmon <henrikillsit@gmail.com>
2026-03-23 13:09:38 -0600

    changed hardcoded urls

--- route-handlers/handleEncode.js
+++ route-handlers/handleEncode.js
@@ -8,15 +8,15 @@ const rotorIII = require('../original-rotors/rotorIII.js')
 let rotorArray = [rotorI, rotorII, rotorIII];
 
 const handleEncode = (request, response) => {
-    try {
-        rotorArray = rotorRequestReset(rotorArray)
-        const input = request.query.message;
-        // console.log('arr in encode req', rotorArray[0].positions)
-        const encodedMessage = handleMultipleCharacters(input, rotorArray);
-        response.status(200).send(`Your encode message ==> ${encodedMessage} <br> Copy the below url into your browser to see the decoded version <br>https://chances-enigma-server.herokuapp.com/decode?message=${encodedMessage}`)
-    } catch (err) {
-        response.status(500).send('Something went wrong, what did Chance do?!')
-    }
+  try {
+    rotorArray = rotorRequestReset(rotorArray)
+    const input = request.query.message;
+    // console.log('arr in encode req', rotorArray[0].positions)
+    const encodedMessage = handleMultipleCharacters(input, rotorArray);
+    response.status(200).send(`Your encoded message ==> ${encodedMessage} <br>To decode it, click here:<br><a href="/decode?message=${encodedMessage}">Decode message</a>`)
+  } catch (err) {
+    response.status(500).send('Something went wrong, what did Chance do?!')
+  }
 }
 
 module.exports = handleEncode;
--- route-handlers/handleHome.js
+++ route-handlers/handleHome.js
@@ -2,11 +2,11 @@
 
 
 const handleHome = (request, response) => {
-    try {
-        response.status(200).send('Thank you for visiting! To test please follow these steps: <br> Paste the following url to encode a message ==> https://chances-enigma-server.herokuapp.com/encode?message=your message here <br> ie ==> https://chances-enigma-server.herokuapp.com/encode?message=enigma');
-    } catch (err) {
-        response.status(500).send('Something went wrong, what did Chance do?!')
-    }
+  try {
+    response.status(200).send('Thank you for visiting! To test the encoder, add a message to the URL like this:<br>/encode?message=your message here<br> Example:<br> /encode?message=enigma');
+  } catch (err) {
+    response.status(500).send('Something went wrong, what did Chance do?!')
+  }
 }
 
 module.exports = handleHome
