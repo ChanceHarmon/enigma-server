47614cf72be78301ef5c38bb10c4e3d9a011b776
Chance Harmon <henrikillsit@gmail.com>
2026-03-23 13:19:26 -0600

    changed hardcoded urls, again

--- route-handlers/handleDecode.js
+++ route-handlers/handleDecode.js
@@ -9,15 +9,15 @@ const rotorIII = require('../original-rotors/rotorIII.js');
 let rotorArray = [rotorI, rotorII, rotorIII];
 
 const handleDecode = (request, response) => {
-    try {
-        const input = request.query.message.toUpperCase();
-        rotorArray = rotorRequestReset(rotorArray)
-        // console.log('in decode route', input)
-        const decodedMessage = handleDecoding(input, rotorArray);
-        response.status(200).send(`Your decoded message ==> ${decodedMessage} <br> Visit the homepage to get started with another encoded message <br> https://chances-enigma-server.herokuapp.com/`)
-    } catch (err) {
-        response.status(500).send('Something went wrong, what did Chance do?!')
-    }
+  try {
+    const input = request.query.message.toUpperCase();
+    rotorArray = rotorRequestReset(rotorArray)
+    // console.log('in decode route', input)
+    const decodedMessage = handleDecoding(input, rotorArray);
+    response.status(200).send(`Your decoded message ==> ${decodedMessage} <br><a href="/">Return to homepage</a>`)
+  } catch (err) {
+    response.status(500).send('Something went wrong, what did Chance do?!')
+  }
 }
 
 module.exports = handleDecode;
